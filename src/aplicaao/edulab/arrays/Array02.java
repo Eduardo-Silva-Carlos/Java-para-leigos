8160758ac4db806805057678258fe135ee33b711
Carlos Eduardo <carloseduard.s7@gmail.com>
2026-07-01 15:29:58 -0300

    Array list

---
+++ src/aplicaao/edulab/arrays/Array02.java
@@ -0,0 +1,29 @@
+package aplicaao.edulab.arrays;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Array02 {
+    public static void main(String[] args) {
+        ArrayList<String> cronograma = new ArrayList<String>(10);
+
+        cronograma.add("Matemática");
+        cronograma.add("Linguagem Formais");
+        cronograma.add("Lógica de programação");
+        cronograma.add("Inglês");
+        cronograma.add("Português");
+        cronograma.add("Java");
+        cronograma.add("Orientação a objetos");
+        cronograma.add("System Design");
+        cronograma.add("Matemática discreta");
+
+        Iterator it = cronograma.iterator();
+
+        while (it.hasNext()){
+            String aux = (String) it.next();
+            System.out.println(aux);
+        }
+
+
+    }
+}
